dbe9e484afc2eb0af69dfd92eb9087e46cd0f1f5
Chris Dart <technology@fsmn.org>
2012-07-25 11:30:33 -0500

    minor update, bug fix to teacher_model

--- application/models/teacher_model.php
+++ application/models/teacher_model.php
@@ -12,7 +12,7 @@ class Teacher_model extends CI_Model
 	var $gradeEnd;
 	var $is_advisor;
 	var $status;
-	var $db_role;
+	var $dbRole;
 	var $recModified;
 	var $recModifier;
 
@@ -28,7 +28,7 @@ class Teacher_model extends CI_Model
 	function prepare_variables()
 	{
 
-		$variables = array("teachFirst","teachLast","teachClass","username","email","gradeStart","gradeEnd","is_advisor","status","db_role");
+		$variables = array("teachFirst","teachLast","teachClass","username","email","gradeStart","gradeEnd","is_advisor","status","dbRole");
 		for($i = 0; $i < count($variables); $i++){
 			$myVariable = $variables[$i];
 			if($this->input->post($myVariable)){
