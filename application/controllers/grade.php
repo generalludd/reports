c049166d24a4138994e5cc711a87045577c5218f
Chris Dart <technology@fsmn.org>
2012-10-09 10:23:34 -0500

    Refactoring Grades
    added comments and set up error capturing for missing get, post and uri
    variables.

--- application/controllers/grade.php
+++ application/controllers/grade.php
@@ -10,57 +10,74 @@ class Grade extends MY_Controller
 		$this->load->model("assignment_model","assignment");
 	}
 
+	function index()
+	{
+		redirect();
+	}
+	/**
+	 * editing the grades for a given student, term, subject, teacher
+	 * the query script get_for_student in this context returns all the assignments possible for easy editing as a chart.
+	 */
 	function edit()
 	{
 		$this->load->model("menu_model");
 		$kStudent = $this->input->get_post("kStudent");
-
 		$kTeach = $this->input->get_post("kTeach");
+		if($kStudent && $kTeach){
+			$year = $this->input->get_post("year");
+			if(!$year){
+				$year = get_current_year();
+			}
+			$term = $this->input->get_post("term");
+			if(!$term){
+				$term = get_current_term();
+			}
+			$options["grade_range"]["gradeStart"] = $this->input->get_post("gradeStart");
+			if(!$options["grade_range"]["gradeStart"]){
+				$options["grade_range"]["gradeStart"] = $this->session->userdata("gradeStart");
+			}
+			$options["grade_range"]["gradeEnd"] = $this->input->get_post("gradeEnd");
+			if(!$options["grade_range"]["gradeEnd"]){
+				$options["grade_range"]["gradeEnd"] = $this->session->userdata("gradeEnd");
+			}
+			$footnotes = $this->menu_model->get_pairs("grade_footnote");
+			$data["footnotes"] = get_keyed_pairs($footnotes, array("value","label"),TRUE);
+			$status = $this->menu_model->get_pairs("grade_status");
 
-		$year = $this->input->get_post("year");
-		if(!$year){
-			$year = get_current_year();
-		}
-		$term = $this->input->get_post("term");
-		if(!$term){
-			$term = get_current_term();
-		}
-		$options["grade_range"]["gradeStart"] = $this->input->get_post("gradeStart");
-		if(!$options["grade_range"]["gradeStart"]){
-			$options["grade_range"]["gradeStart"] = $this->session->userdata("gradeStart");
-		}
-		$options["grade_range"]["gradeEnd"] = $this->input->get_post("gradeEnd");
-		if(!$options["grade_range"]["gradeEnd"]){
-			$options["grade_range"]["gradeEnd"] = $this->session->userdata("gradeEnd");
-		}
-		$footnotes = $this->menu_model->get_pairs("grade_footnote");
-		$data["footnotes"] = get_keyed_pairs($footnotes, array("value","label"),TRUE);
-		$status = $this->menu_model->get_pairs("grade_status");
+			$data["status"] = get_keyed_pairs($status, array("value","label"),TRUE);
+			$data["kStudent"] = $kStudent;
+			$data["kTeach"] = $kTeach;
+			$options["kTeach"] = $kTeach;
+			$data["grades"] = $this->assignment->get_for_student($kStudent,$term,$year,$options);
 
-		$data["status"] = get_keyed_pairs($status, array("value","label"),TRUE);
-		$data["kStudent"] = $kStudent;
-		$data["kTeach"] = $kTeach;
-		$options["kTeach"] = $kTeach;
-		$data["grades"] = $this->assignment->get_for_student($kStudent,$term,$year,$options);
-
-		$this->load->view("grade/edit",$data);
-
+			$this->load->view("grade/edit",$data);
+		}
 
 	}
 
+	/**
+	 * edit_cell allows editing of a single grade in a chart for a given student.
+	 */
 	function edit_cell()
 	{
 		$kAssignment = $this->input->get("kAssignment");
 		$kStudent = $this->input->get("kStudent");
-		$this->load->model("menu_model");
-		$data["grade"] = $this->grade->get($kStudent,$kAssignment);
-		$footnotes = $this->menu_model->get_pairs("grade_footnote");
-		$data["footnotes"] = get_keyed_pairs($footnotes, array("value","label"),TRUE);
-		$status = $this->menu_model->get_pairs("grade_status");
-		$data["status"] = get_keyed_pairs($status, array("value","label"),TRUE);
-		$this->load->view("grade/edit_cell",$data);
+		if($kAssignment && $kStudent){
+			$this->load->model("menu_model");
+			$data["grade"] = $this->grade->get($kStudent,$kAssignment);
+			$footnotes = $this->menu_model->get_pairs("grade_footnote");
+			$data["footnotes"] = get_keyed_pairs($footnotes, array("value","label"),TRUE);
+			$status = $this->menu_model->get_pairs("grade_status");
+			$data["status"] = get_keyed_pairs($status, array("value","label"),TRUE);
+			$this->load->view("grade/edit_cell",$data);
+		}
 	}
 
+
+
+	/**
+	 * select_student offers a selection option to find a student to add to a grade chart.
+	 */
 
 	function select_student()
 	{
@@ -72,23 +89,32 @@ class Grade extends MY_Controller
 		$this->load->view("student/mini_selector",$data);
 	}
 
+
+	/**
+	 * update also inserts (through the update script in grade_method).
+	 */
 	function update()
 	{
 		$kStudent = $this->input->post("kStudent");
 		$kAssignment = $this->input->post("kAssignment");
-		$assignment = $this->assignment->get($kAssignment);
-		//$total points is needed to calculate the grade average.
-		//this calculation happens in the model to make more elegant code.
-		$total = $assignment->points;
-		$kTeach = $assignment->kTeach;
-		$category = $assignment->category;
-		$points = $this->input->post("points");
-		$status = $this->input->post("status");
-		$footnote = $this->input->post("footnote");
-		$result = $this->grade->update($kStudent,$kAssignment,$kTeach, $points,$total,$status,$footnote,$category);
-		echo OK;
+		if($kStudent && $kAssignment){
+			$assignment = $this->assignment->get($kAssignment);
+			//$total points is needed to calculate the grade average.
+			//this calculation happens in the model to make more elegant code.
+			$total = $assignment->points;
+			$kTeach = $assignment->kTeach;
+			$category = $assignment->category;
+			$points = $this->input->post("points");
+			$status = $this->input->post("status");
+			$footnote = $this->input->post("footnote");
+			$result = $this->grade->update($kStudent,$kAssignment,$kTeach, $points,$total,$status,$footnote,$category);
+			echo OK;
+		}
 	}
 
+	/**
+	 * select_report_card provides a dialog for selecting the report card for a given student based on year, term, cutoff-date, and subject
+	 */
 	function select_report_card()
 	{
 		if($kStudent = $this->input->get("kStudent")){
@@ -103,70 +129,75 @@ class Grade extends MY_Controller
 		}
 	}
 
+	/**
+	 * report_card displays a report card based on submitted criteria for year, term, cutoff-date, and subject.
+	 */
 	function report_card(){
-		$kStudent = $this->input->get("kStudent");
-		$this->load->model("student_model","student");
-		$kTeach = NULL;
-		$options = array();
-		$options["from"] = "grade";
-		$options["join"] = "assignment";
-		if($kTeach = $this->input->get("kTeach")){
-			$options["kTeach"] = $kTeach;
-		}
-		$output["cutoff_date"] = FALSE;
-		if($cutoff_date = $this->input->get("cutoff_date")){
-			$options["cutoff_date"] = format_date($cutoff_date,"mysql");
-			$output["cutoff_date"] = $cutoff_date;
-		}
+		if($kStudent = $this->input->get("kStudent")){
+			$this->load->model("student_model","student");
+			$kTeach = NULL;
+			$options = array();
+			$options["from"] = "grade";
+			$options["join"] = "assignment";
+			if($kTeach = $this->input->get("kTeach")){
+				$options["kTeach"] = $kTeach;
+			}
+			$output["cutoff_date"] = FALSE;
+			if($cutoff_date = $this->input->get("cutoff_date")){
+				$options["cutoff_date"] = format_date($cutoff_date,"mysql");
+				$output["cutoff_date"] = $cutoff_date;
+			}
 
-		$term = get_current_term();
-		if($this->input->get("term")){
-			$term = $this->input->get("term");
-		}
-		$output["term"] = $term;
-		
-		
-		$year = get_current_year();
-		if($this->input->get("year")){
-			$year = $this->input->get("year");
-		}
-		$output["year"] = $year;
+			$term = get_current_term();
+			if($this->input->get("term")){
+				$term = $this->input->get("term");
+			}
+			$output["term"] = $term;
 
-		if($subject = $this->input->get("subject")){
-			$subjects = array($subject);
-		}else{
-			$subjects = $this->grade->get_subjects($kStudent,$term,$year);
-		}
-		$data["target"] = "grade/report_card";
-		$data["title"] = "Report Card";
 
-		$student = $this->student->get($kStudent);
-		$output["student"] =  format_name($student->stuFirst, $student->stuLast, $student->stuNickname);
-		$output["charts"] = array();
+			$year = get_current_year();
+			if($this->input->get("year")){
+				$year = $this->input->get("year");
+			}
+			$output["year"] = $year;
 
-		if(count($subjects) == 1){
-			$options["subject"] = $subject;
-			$data["subject"] = $subject;
-			$data["grades"] = $this->assignment->get_for_student($kStudent,$term,$year,$options);
-			$data["categories"] = $this->grade->get_categories($kStudent, $term, $year,$options);
-			$output["charts"][] = $this->load->view("grade/chart",$data,TRUE);
+			if($subject = $this->input->get("subject")){
+				$subjects = array($subject);
+			}else{
+				$subjects = $this->grade->get_subjects($kStudent,$term,$year);
+			}
+			$data["target"] = "grade/report_card";
+			$data["title"] = "Report Card";
 
-		}else{
-			foreach($subjects as $subject){
-				$options["subject"] = $subject->subject;
-				$data["subject"] = $subject->subject;
+			$student = $this->student->get($kStudent);
+			$output["student"] =  format_name($student->stuFirst, $student->stuLast, $student->stuNickname);
+			$output["charts"] = array();
+			//if a subject has been submitted we change the query to one item.
+			//This could be cleaned up by instantiating an temporary "subject" object
+			//with one attribute (subject = submitted subject);
+			if(count($subjects) == 1){
+				$options["subject"] = $subject;
+				$data["subject"] = $subject;
 				$data["grades"] = $this->assignment->get_for_student($kStudent,$term,$year,$options);
 				$data["categories"] = $this->grade->get_categories($kStudent, $term, $year,$options);
 				$output["charts"][] = $this->load->view("grade/chart",$data,TRUE);
+
+			}else{
+				foreach($subjects as $subject){
+					$options["subject"] = $subject->subject;
+					$data["subject"] = $subject->subject;
+					$data["grades"] = $this->assignment->get_for_student($kStudent,$term,$year,$options);
+					$data["categories"] = $this->grade->get_categories($kStudent, $term, $year,$options);
+					$output["charts"][] = $this->load->view("grade/chart",$data,TRUE);
+				}
 			}
+
+			if($output){
+				$this->load->view("page/index",$output);
+			}
+
+
+
 		}
-
-		if($output){
-			$this->load->view("page/index",$output);
-		}
-
-
-
 	}
-
 }
