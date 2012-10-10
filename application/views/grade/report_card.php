f329d8d4b558834bb1806ed404672f0df6fea0a1
Chris Dart <technology@fsmn.org>
2012-10-10 12:16:05 -0500

    rearranged output of grade report cards for better printing

--- application/views/grade/report_card.php
+++ application/views/grade/report_card.php
@@ -1,16 +1,31 @@
 <?php 
 $this->load->view("student/navigation", array("kStudent",$kStudent));
-
+$i = 0;
 ?>
 
-<h2>Report Card for <?=$student_name;?></h2>
+
 <div id='report-card'>
-<? if($cutoff_date):?>
-	 <div class='report-header report-cutoff'>For grades given through <?=$cutoff_date;?></div>
-<? endif;?>
-<div class='report-header report-term'><?="$term, $year";?></div>
-<? foreach($charts as $chart){
-	echo $chart;
-} ?>
+
+	<? foreach($charts as $chart): ?>
+	<div class='report-chart report-<?=$i;?>'>
+		<h2>
+			Report Card for
+			<?=$student_name;?>
+		</h2>
+		<div class='report-header report-term'>
+			<?="$term, $year";?>
+		</div>
+
+		<? if($cutoff_date):?>
+		<div class='report-header report-cutoff'>
+			For grades given through
+			<?=$cutoff_date;?>
+		</div>
+		<? endif;?>
+		<?=$chart; ?>
+	</div>
+		<? $i++; //iterate to row numbers?>
+	
+	<? endforeach; ?>
 
 </div>
