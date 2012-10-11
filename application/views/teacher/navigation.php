a0db96fad783721929f2561ba58cd316a32f688b
Chris Dart <technology@fsmn.org>
2012-10-11 10:05:56 -0500

    Overhauled teacher navigation
    Removed redundant buttons for teacher users. Now buttons visible for
    editors as needed.

--- application/views/teacher/navigation.php
+++ application/views/teacher/navigation.php
@@ -1,23 +1,21 @@
 <?php
 $buttons = array();
-if($teacher->dbRole == 2){
-
-	$buttons[] = array("selection" => "student/teacher_student_list", "href"=>site_url("student/teacher_student_list/$kTeach"), "text"=>"List Students");
+if($this->session->userdata("dbRole") == 1){
 	$buttons[] = array("selection" => "narrative", "type" => "span", "text" => "List Narratives", "class"=>array("button","teacher_narrative_search"),"id"=>"tns_$kTeach");
-
-	$buttons[] = array("selection" => "narrative/teacher_list", "href"=> site_url("narrative/teacher_list/$kTeach/print"), "text" => "Print Narratives" );
 	$buttons[] = array("selection" => "template/list_templates", "href"=>  site_url("template/list_templates/?kTeach=$kTeach&term=$term&year=$year") , "text" => "Subject Templates" );
-	if($teacher->is_advisor == 1){
-		$buttons[] = array("selection"=>"report/get_list/advisor", "href" =>site_url("report/get_list/advisor/$kTeach"),"text" => sprintf("%ss",STUDENT_REPORT));
+	if($teacher->gradeEnd > 4){
+		$buttons[] = array("selection" => "assignment", "text" => "Grades","class"=> array("button","search-assignments"),"id" =>"sa_$kTeach","title" => "Search for current grade charts");
 	}
-
+	$buttons[] = array("selection" => "student/teacher_student_list", "href"=>site_url("student/teacher_student_list/$kTeach"), "text"=>"List Students");
+	$buttons[] = array("selection" => "narrative/teacher_list", "href"=> site_url("narrative/teacher_list/$kTeach/print"), "text" => "Print Narratives" );
+	
+}
+if($teacher->dbRole == 2 && $teacher->is_advisor == 1){
+		$buttons[] = array("selection"=>"report/get_list/advisor", "href" =>site_url("report/get_list/advisor/$kTeach"),"text" => sprintf("%ss",STUDENT_REPORT));
 }
 $buttons[] = array("selection" =>"report/get_list/teacher","href"=>site_url("report/get_list/teacher/$kTeach"),"text"=> sprintf("Submitted %ss", STUDENT_REPORT));
-if($teacher->gradeEnd > 4){
-	$buttons[] = array("selection" => "assignment", "text" => "Grades","class"=> array("button","search-assignments"),"id" =>"sa_$kTeach","title" => "Search for current grade charts");
-}
 
-$buttons[] = array("selection"=>"teacher/view","text"=>"My Account","href"=>site_url("teacher/view/$kTeach"));
+$buttons[] = array("selection"=>"teacher/view","text"=>"Account Info","href"=>site_url("teacher/view/$kTeach"));
 
 $options["selection"] = $this->uri->segment(1);
 $options["id"] = "teacher-buttons";
