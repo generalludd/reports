23a8708a37ab81c4095f5c73ca37e54a4de0bf27
Chris Dart <technology@fsmn.org>
2012-10-22 09:42:08 -0500

    Added color css to login screen
    forgot that I removed the @import color.css from the main.css.

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -5,6 +5,8 @@
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 <title>Narrative Reporting System Login</title>
 <link href="<?=base_url();?>/css/main.css" type="text/css" rel="stylesheet" media="all" />
+<link href="<?=base_url();?>/css/color.css" type="text/css" rel="stylesheet" media="all" />
+
 <script type="text/javascript" src="<?=base_url();?>js/jquery.min.js"></script>
 <script type="text/javascript" src="<?=base_url();?>js/password.js"></script>
 </head>
