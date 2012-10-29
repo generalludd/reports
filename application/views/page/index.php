96f0a605b25d4014a6087a1616fa32a4100f3bc5
Chris Dart <technology@fsmn.org>
2012-10-29 11:41:33 -0500

    Removing pesky html annotations that may or may not be necessary

--- application/views/page/index.php
+++ application/views/page/index.php
@@ -7,10 +7,9 @@ $body_class = $this->uri->segment(1);
 if($this->uri->segment(1) == ""){
 	$body_class = "front";
 }
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en' >
+<html>
 <head>
 <? $this->load->view('page/head');?>
 </head>
