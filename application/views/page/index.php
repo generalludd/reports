e529f90b5ffa43f8053769bf10748c71447cce4b
Chris Dart <technology@fsmn.org>
2012-10-29 11:35:59 -0500

    Adjustments to xhtml headers for transitional instead of strict

--- application/views/page/index.php
+++ application/views/page/index.php
@@ -9,8 +9,8 @@ if($this->uri->segment(1) == ""){
 }
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html <?="xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'";?> >
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en' >
 <head>
 <? $this->load->view('page/head');?>
 </head>
