90a653a3f38fb8033d3f8308b48c6046418670c2
Chris Dart <technology@fsmn.org>
2013-12-20 12:31:09 -0600

    Proper formatting and improvements to grade_preference editor

--- application/views/grade_preference/edit.php
+++ application/views/grade_preference/edit.php
@@ -1,5 +1,16 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<input type="hidden" name= "kStudent" id="kStudent" value="<?=$preference->kStudent;?>"/>
-<input type="text" name="subject" id="subject" value="<?=$preference->subject;?>"/>
-<input type="text" name="school_year" id="school_year" value="<?=$preference->school_year;?>"/>
-<input type="text" name="pass_fail" id="pass_fail" value="<?=$preference->pass_fail;?>"/>
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+$year = get_value($preference, "school_year",get_current_year());
+$pass_fail_check ="";
+if(get_value($preference,"pass_fail") == 1){
+	$pass_fail_check = "checked";
+}
+?>
+<form name="grade-preference-editor" id="grade-preference-editor" action="<?=site_url("grade_preference/$action");?>" method="post">
+<input type="hidden" name= "kStudent" id="kStudent" value="<?=$kStudent;?>"/>
+<?=form_dropdown("subject", $subjects, get_value($preference,"subject"), "id='subject'");?><br/>
+<input type="text" style="width:5ex" name="school_year" id="school_year" value="<?=$year;?>"/>
+-<?=$year + 1;?><br/>
+<label for="pass_fail">Is Pass Fail?</label>
+<input type="checkbox" name="pass_fail" id="pass_fail" value="1" <?=$pass_fail_check;?>/><br/>
+<input type="submit" name="submit" value="<?=ucfirst($action);?>"/>
+</form>
