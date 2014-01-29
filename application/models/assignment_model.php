d83cf0baca6c5d4bd5b2ac8959bcbbdc4fb93ba7
Chris Dart <technology@fsmn.org>
2014-01-29 11:53:48 -0600

    refactoring and adding more phpdoc explanation.

--- application/models/assignment_model.php
+++ application/models/assignment_model.php
@@ -1,5 +1,4 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 class Assignment_model extends CI_Model
 {
