bb74d85e9441eb4b6db42ce8a43b0382df023d0e
Chris Dart <technology@fsmn.org>
2014-06-10 11:17:29 -0500

    adding cleanup and code options to wysiwyg editor

--- js/editor.js
+++ js/editor.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 			plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
 			gecko_spellcheck: true,
 			// Theme options
-			theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,fullscreen,|,cut,copy,paste,pastetext,pasteword,|,undo,redo,|,bullist,numlist",
+			theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,fullscreen,|,cut,copy,paste,pastetext,pasteword,cleanup,code,|,undo,redo,|,bullist,numlist",
 			theme_advanced_buttons2 : "", //"cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
 			theme_advanced_buttons3 : "",
 			theme_advanced_buttons4 : "",//"insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
@@ -18,7 +18,7 @@ $(document).ready(function() {
 			theme_advanced_resizing : true,
 
 			// Example content CSS (should be your site CSS)
-//			content_css : "css/main.css",
+			content_css : root_url + "/css/main.css",
 
 			// Drop lists for link/image/media/template dialogs
 			template_external_list_url : "lists/template_list.js",
