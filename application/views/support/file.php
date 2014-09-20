242e1462e5fb77199a58de4bcb0ddbcd2692b43d
Chris Dart <technology@fsmn.org>
2014-09-19 19:03:30 -0500

    Cleaning up interface for Student Support

--- application/views/support/file.php
+++ application/views/support/file.php
@@ -1,9 +1,9 @@
 <?php
 
 ?>
-<div style="width:350px">
-<div class='notice'>As much of content of attached files should also be entered in the text fields of this special needs summary.</div>
-<form method="post" enctype="multipart/form-data" name="support-file-editor" id="support-file-editor" action="<?=site_url("support/attach_file");?>">
+<div style="width:240px">
+<div class='notice'>Please make sure that as much of the information in this file is also entered in the special needs form fields.</div>
+<form style="width:90%;margin:0 auto;" method="post" enctype="multipart/form-data" name="support-file-editor" id="support-file-editor" action="<?=site_url("support/attach_file");?>">
 <input type="hidden" name="kSupport" id="kSupport" value="<?=$kSupport?>"/>
 <input type="hidden" name="kStudent" id="kStudent" value="<?=$kStudent;?>"/>
 <input type="hidden" name="kFile" id="kFile" value="<?=get_value($file,'kFile');?>"/>
