ec4aaafb0677cb2e6ef8123b0b68bda68e8b6448
Chris Dart <technology@fsmn.org>
2015-06-09 13:44:48 -0500

    More updates to allow editors to add new learning support entries for the next year before the end of the current school year.

--- application/views/support/file.php
+++ application/views/support/file.php
@@ -1,19 +1,25 @@
 <?php
-
 ?>
-<div style="width:240px">
-<div class='notice'>Please make sure that as much of the information in this file is also entered in the special needs form fields.</div>
-<form style="width:90%;margin:0 auto;" method="post" enctype="multipart/form-data" name="support-file-editor" id="support-file-editor" action="<?=site_url("support/attach_file");?>">
-<input type="hidden" name="kSupport" id="kSupport" value="<?=$kSupport?>"/>
-<input type="hidden" name="kStudent" id="kStudent" value="<?=$kStudent;?>"/>
-<input type="hidden" name="kFile" id="kFile" value="<?=get_value($file,'kFile');?>"/>
-<p><label for="file_display_name">Display Name</label><br />
-<input type="text" name="file_display_name" id="file_display_name" value="" /></p>
-<p><label for="file_description">Description</label><br />
-<input type="text" name="file_description" id="file_description" value="" /></p>
-<p>
-	<input type="file" name="userfile" class="" size="20" /></p>
-<p><span class="button attach-support-file">Attach</span>
-</p>
-</form>
-<div>
+<div style="width: 240px">
+	<div class='notice'>Please make sure that as much of the information in this file is also entered in the special needs form fields.</div>
+	<form style="width: 90%; margin: 0 auto;" method="post" enctype="multipart/form-data" name="support-file-editor" id="support-file-editor"
+		action="<?=site_url("support/attach_file");?>">
+		<input type="hidden" name="kSupport" id="kSupport" value="<?=$kSupport?>" />
+		<input type="hidden" name="kStudent" id="kStudent" value="<?=$kStudent;?>" />
+		<input type="hidden" name="kFile" id="kFile" value="<?=get_value($file,'kFile');?>" />
+		<p>
+			<label for="file_display_name">Display Name</label><br />
+			<input type="text" name="file_display_name" id="file_display_name" value="" />
+		</p>
+		<p>
+			<label for="file_description">Description</label><br />
+			<input type="text" name="file_description" id="file_description" value="" />
+		</p>
+		<p>
+			<input type="file" name="userfile" class="" size="20" />
+		</p>
+		<p>
+			<span class="button attach-support-file">Attach</span>
+		</p>
+	</form>
+</div>
--- application/views/support/view.php
+++ application/views/support/view.php
@@ -1,90 +1,116 @@
-<?php #support/view.php
-
+<?php
+// support/view.php
 $had_meeting = "No";
 
-
-if($entry->meeting == 1){
-    $had_meeting = "Yes";
+if ($entry->meeting == 1) {
+	$had_meeting = "Yes";
 }
 
- $has_iep = "No";
- 
-if($entry->hasIEP == 1){
-   $has_iep  = "Yes";
+$has_iep = "No";
+
+if ($entry->hasIEP == 1) {
+	$has_iep = "Yes";
 }
 
 $has_spps = "No";
 
-if( $entry->hasSPPS == 1 ){
-    $has_spps = "Yes";
+if ($entry->hasSPPS == 1) {
+	$has_spps = "Yes";
 }
-$files_array = array();
-if($support_files){
-	foreach($support_files as $file_item){
-		if($file_item->kSupport == $entry->kSupport){
-			$files_array[] = $file_item;
+$files_array = array ();
+if ($support_files) {
+	foreach ( $support_files as $file_item ) {
+		if ($file_item->kSupport == $entry->kSupport) {
+			$files_array [] = $file_item;
 		}
 	}
 }
 
-
 $year = $entry->year;
 $year_end = $year + 1;
 $test_date = $entry->testDate;
-if($test_date){
-    $test_date = format_date($test_date, "standard");
-}else{
-    $test_date = "None";
+if ($test_date) {
+	$test_date = format_date ( $test_date, "standard" );
+} else {
+	$test_date = "None";
 }
 
-if($print){
+if ($print) {
 	echo "<h2>Learning Support for $student</h2>";
 }
 
-
-
 ?>
 <fieldset class="support-view">
-<legend><?=$year . "-" . $year_end?></legend>
-<? if(!$print){
-	
-	$buttons[] = array("selection" => "support", "href" => site_url("support/edit/$entry->kSupport"), "text" => "Edit", "class" => "button edit");
-	
-	$buttons[] = array("selection" => "print", "href" => site_url("support/view/$entry->kSupport/print"),"target" => "_blank", "text" => "Print");
-	print create_button_bar($buttons);
-	
-}?>
-<h4>Diagnosis/Description</h4>
-<p><?=$entry->specialNeed;?></p>
-<p>Has had fall meeting: <strong><?=$had_meeting;?></strong></p>
-<p>Test Date: <strong><?=$test_date;?></strong></p>
+	<legend><?=$year . "-" . $year_end?></legend>
+<?
 
-<h4>Outside Support/Treatments</h4>
-<div><?=$entry->outsideSupport?></div>
-<p>IEP on File: <strong><?=$has_iep;?></strong></p>
-<p>Saint Paul Public Schools Support: <strong><?=$has_spps;?></strong></p>
+if (! $print) {
+	
+	$buttons [] = array (
+			"selection" => "support",
+			"href" => site_url ( "support/edit/$entry->kSupport" ),
+			"text" => "Edit",
+			"class" => "button edit" 
+	);
+	
+	$buttons [] = array (
+			"selection" => "print",
+			"href" => site_url ( "support/view/$entry->kSupport/print" ),
+			"target" => "_blank",
+			"text" => "Print" 
+	);
+	print create_button_bar ( $buttons );
+}
+?>
+<h4>Diagnosis/Description</h4>
+	<p><?=$entry->specialNeed;?></p>
+	<p>
+		Has had fall meeting: <strong><?=$had_meeting;?></strong>
+	</p>
+	<p>
+		Test Date: <strong><?=$test_date;?></strong>
+	</p>
+
+	<h4>Outside Support/Treatments</h4>
+	<div><?=$entry->outsideSupport?></div>
+	<p>
+		IEP on File: <strong><?=$has_iep;?></strong>
+	</p>
+	<p>
+		Saint Paul Public Schools Support: <strong><?=$has_spps;?></strong>
+	</p>
 <? if($entry->modification): ?>
 <h4>Accommodations</h4>
-<div><?=$entry->modification; ?></div>
+	<div><?=$entry->modification; ?></div>
 <? endif;?>
 
 <? if(!empty($files_array) && !$print):?>
 <div class='file-attachments'>
-<h4 >File Attachments</h4>
-<table class="list files">
-	<thead>
-		<tr>
-			<th><strong>File Name</strong></th>
-			<th><strong>Description</strong></th>
-		</tr>
-	</thead>
-	<tbody>
+		<h4>File Attachments</h4>
+		<table class="list files">
+			<thead>
+				<tr>
+					<th>
+						<strong>File Name</strong>
+					</th>
+					<th>
+						<strong>Description</strong>
+					</th>
+				</tr>
+			</thead>
+			<tbody>
 	<? foreach($files_array as $file):?>
-	<tr><td class='file-name'><a href='<?=base_url() ."uploads/$file->file_name";?>' target='_blank'><?=$file->file_display_name;?></a></td>
-	<td class='file-description'><?=$file->file_description;?></td></tr>
+	<tr>
+					<td class='file-name'>
+						<a href='<?=base_url() ."uploads/$file->file_name";?>' target='_blank'><?=$file->file_display_name;?></a>
+					</td>
+					<td class='file-description'><?=$file->file_description;?></td>
+				</tr>
 	<? endforeach; ?>
 	</tbody>
-</table>
-<?	endif; ?></div>
+		</table>
+	</div>
+		
+<?	endif; ?>
 
 </fieldset>
