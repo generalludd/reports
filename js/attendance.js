5f486400c39bc045f22fc9d2473b8399cc627b5c
Chris Dart <technology@fsmn.org>
2015-07-07 15:19:19 -0500

    clean up after removing specific ajax delete functions--move toward uniform delete options.

--- js/attendance.js
+++ js/attendance.js
@@ -1,29 +1,5 @@
 $(document).ready(function(){
 	
-
-	$('.delete_attendance').live('click',
-		function(e){
-		e.preventDefault();
-				$("#action").val("delete");
-				choice=confirm("Are you absolutely sure you want to delete this attendance entry? This cannot be undone!");
-				if(choice) {
-					document.forms["attendanceEditor"].submit();
-				}else{
-					return false;
-				}
-			}//end function(event)
-	);//end click
-	
-	$('.search_attendance').live('click',function(e){
-		e.preventDefault();
-		 var startDate=$('#startDate').val();
-		 var endDate=$('#endDate').val();
-		 if(startDate=="" || endDate==""){
-			 alert("Start and End dates are Required!");
-		 }else{
-			 document.forms['attendance_search'].submit();
-		 }
-	});
 	$("#attendType").live("change", function(){
 		$("#attend-length-notice").html("Please make sure to identify the length of the absence").addClass("notice");
 		if($("#attendType").val() == "Appointment"){
