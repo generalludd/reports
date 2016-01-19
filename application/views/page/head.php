ac1770ed2e6d44e2d7610c34a284310b81c22b31
chrisdart <chrisd@fsmn.org>
2016-01-19 17:48:01 -0600

    clean up unncessary js code.

--- application/views/page/head.php
+++ application/views/page/head.php
@@ -19,7 +19,6 @@ var root_url = '<?=base_url();?>';
 <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
 <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.3.0.min.js"></script>
 <script type="text/javascript" src="https://code.jquery.com/ui/1.8.24/jquery-ui.min.js"></script>
-<script type="text/javascript" src="<?=base_url("js/cookie.jquery.js");?>"></script>
 
 <!-- General Script  -->
 <script type="text/javascript" src="<?=base_url("js/general.js");?>"></script>
