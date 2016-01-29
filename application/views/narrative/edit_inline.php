64a51e6118e7aa54ce97b84b5c1f130d82127cf8
chrisdart <chrisd@fsmn.org>
2016-01-29 15:27:28 -0600

    more tweaks to rich text editing

--- application/views/narrative/edit_inline.php
+++ application/views/narrative/edit_inline.php
@@ -1,10 +1,6 @@
 <?php
 ?>
-<script src="//tinymce.cachefly.net/4.3/tinymce.min.js"></script>
 
-<script>
-
-</script> 
 <form id="narrative_inline_editor" name="narrative_inline_editor"
 	action="<?=site_url("narrative/update_inline");?>" method="post">
 	<input type="hidden"
@@ -16,3 +12,21 @@
 	style="width: 99.75%;" rows="19" cols="107"><?=stripslashes($narrative->narrText);?></textarea></div>
 <p><span class="button new save_narrative_inline">Save</span>&nbsp;<a href="#" class="enable-rich-text button" id="rtf_<?php echo $narrative->kNarrative;?>">Enable Rich Text</a></p>
 </form>
+<script>
+tinymce.init({ selector:'textarea.tinymce',
+	menubar: false,
+	min_height:400,
+	toolbar: 'bold,italic,|,fullpage,|,cut,copy,paste,pastetext,pasteword,cleanup,code,|,undo,redo,|,bullist,numlist',
+	invalid_styles: 'color font-size font-family line-height font-weight',
+	plugins: 'fullpage,paste,code',
+	invalid_elements: 'div,font,a,html,head,body',
+	setup: function (editor) {
+	        editor.on('change', function () {
+	            editor.save();
+	        });
+	    },
+	    browser_spellcheck: true,
+	    contextmenu: false
+
+	});
+</script> 
