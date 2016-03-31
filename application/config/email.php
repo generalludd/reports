bae8eb3d74d59ab023093aa2f9c960bfb0364f5a
chrisdart <chrisd@fsmn.org>
2016-03-31 16:30:39 -0500

    email.php

--- application/config/email.php
+++ application/config/email.php
@@ -1,8 +1,8 @@
 <?php
-
-$config['mailtype'] =  'html';
-$config['protocol'] = 'sendmail';
-$config['mailpath'] = '/usr/sbin/sendmail';
+$config['protocol'] = "smtp";
 $config['charset'] = 'iso-8859-1';
 $config['wordwrap'] = TRUE;
-
+$config['smtp_host'] = "smtp-relay.gmail.com";
+$config['smtp_user'] = "reports@fsmn.org";
+$config['smtp_port'] = 25;
+$config['smtp_timeout'] = 25;
