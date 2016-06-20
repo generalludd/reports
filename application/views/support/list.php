c82cc6f37747ef28b3f7db4a13a3f20a09285ce4
chrisdart <chrisd@fsmn.org>
2016-06-20 13:28:08 -0500

    Fix support data entry interface for clearer setup for new year.

--- application/views/support/list.php
+++ application/views/support/list.php
@@ -8,15 +8,12 @@ if($this->session->userdata("dbRole")<3){
 <?php 
 $this->load->view("student/navigation", $kStudent);
 if( $has_current == $current_year): ?>
-<fieldset class="support-view">
-<legend>Support Entries for <?php echo format_schoolyear($current_year);?></legend>
-<p><?=$student_name?> alread yas a support entry for <?php echo format_schoolyear($current_year);?>.
+<p><?=$student_name?> alread has a support entry for <?php echo format_schoolyear($current_year);?>.
 <? if($this->session->userdata("dbRole") == 1): ?>
 <?php $next_year = $current_year + 1;?>
 <a class="button new small" href="<?=site_url("support/create/$kStudent/$next_year");?>">Add for Next Year</a>
 <? endif;?>
 </p>
-	</fieldset>
 <? elseif(!$support || $has_current != $current_year): ?>
 <fieldset class="support-view">
 <legend><?=format_schoolyear(get_current_year());?></legend>
