6d3805702dea5f2417ab25138d7c9541de35eef0
chrisdart <chrisd@fsmn.org>
2017-01-30 12:46:36 -0600

    more updates to tinymce button bar. Search/Replace

--- application/views/narrative/edit_inline.php
+++ application/views/narrative/edit_inline.php
@@ -17,9 +17,9 @@ tinymce.init({ selector:'textarea#narrText_' + <?php echo $narrative->kNarrative
 	menubar: false,
 	min_height:400,
 	block_formats:'Paragraph=p;Heading=h3;Subhead=h4;Section=h5;Subsection=h6',
-	toolbar: 'bold,italic,formatselect,bullist,numlist,|,fullpage,|,cut,copy,paste,pastetext,removeformat,code,|,undo,redo',
+	toolbar: 'bold,italic,formatselect,bullist,numlist,|,fullpage,|,cut,copy,paste,pastetext,removeformat,code,|,undo,redo,searchreplace',
 	invalid_styles: 'color font-size font-family line-height font-weight',
-	plugins: 'fullpage,paste,code,lists',
+	plugins: 'fullpage,paste,code,lists,searchreplace,wordcount',
 	invalid_elements: 'div,font,a',
 	setup: function (editor) {
 	        editor.on('change', function () {
--- application/views/page/head.php
+++ application/views/page/head.php
@@ -32,9 +32,9 @@ var root_url = '<?=base_url();?>';
 	menubar: false,
 	min_height:400,
 	block_formats:'Paragraph=p;Heading=h3;Subhead=h4;Section=h5;Subsection=h6',
-	toolbar: 'bold,italic,formatselect,bullist,numlist,|,fullpage,|,cut,copy,paste,pastetext,pasteword,removeformat,code,|,undo,redo',
+	toolbar: 'bold,italic,formatselect,bullist,numlist,|,fullpage,|,cut,copy,paste,pastetext,pasteword,removeformat,code,|,undo,redo,searchreplace',
 	invalid_styles: 'color font-size font-family line-height font-weight',
-	plugins: 'fullpage,paste,code,wordcount,lists',
+	plugins: 'fullpage,paste,code,wordcount,lists,searchreplace',
 	invalid_elements: 'div,font,a,html,head,body',
 	setup: function (editor) {
 	        editor.on('change', function () {
