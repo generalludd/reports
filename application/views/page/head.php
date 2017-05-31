e1845e8f0490285ca32b505eb81f2fdf5eab22a6
chrisdart <chrisd@fsmn.org>
2017-05-31 15:58:06 -0500

    more tinyMCE plugin cleanup

--- application/views/page/head.php
+++ application/views/page/head.php
@@ -36,7 +36,7 @@ var root_url = '<?=base_url();?>';
 	block_formats:'Paragraph=p;Heading=h3;Subhead=h4;Section=h5;Subsection=h6',
 	toolbar: 'bold,italic,formatselect,bullist,numlist,|,cut,copy,paste,pastetext,pasteword,removeformat,code,|,undo,redo,searchreplace',
 	invalid_styles: 'color font-size font-family line-height font-weight',
-	plugins: 'fullpage,paste,code,wordcount,lists,searchreplace',
+	plugins: 'paste,code,wordcount,lists,searchreplace',
 	invalid_elements: 'div,font,a,html,head,body',
 	setup: function (editor) {
 	        editor.on('change', function () {
