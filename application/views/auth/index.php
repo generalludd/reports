8cb1f8ba19b85c5a0dab8a9668749e71d8d51685
Chris Dart <chrisdart@cerebratorium.com>
2017-11-02 20:00:40 -0500

    fix login javascript

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -7,7 +7,9 @@
 <link href="<?php  echo base_url("/css/main.css");?>" type="text/css" rel="stylesheet" media="all" />
 <link href="<?php  echo base_url("/css/color.css");?>" type="text/css" rel="stylesheet" media="all" />
 
-<script type="text/javascript" src="<?php  echo base_url("js/jquery.min.js");?>"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.3.0.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/ui/1.8.24/jquery-ui.min.js"></script>
 <script type="text/javascript" src="<?php  echo base_url("js/password.js");?>"></script>
 </head>
 <body class="not-logged-in <?php  echo $this->uri->segment(1);?>">
