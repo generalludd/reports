d61f977099618e18b2af24d5d78788a40ba06505
chrisdart <chrisd@fsmn.org>
2018-11-14 17:27:09 -0600

    bug fix so always loads new versions of js and css

--- application/views/page/head.php
+++ application/views/page/head.php
@@ -6,19 +6,19 @@
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <title><?php  echo $title;?></title>
 <meta http-equiv="refresh" content = "28800; url=<?php echo site_url("auth/logout");?>">
-<link type="text/css" rel="stylesheet" media="all" href="<?php  echo base_url("css/main.css?") . date("U");?>" />
-<link type="text/css" rel="stylesheet" media="all" href="<?php  echo base_url("css/navigation.css?") . date("U");?>" />
+<link type="text/css" rel="stylesheet" media="all" href="<?php  echo base_url("css/main.css?");?>" />
+<link type="text/css" rel="stylesheet" media="all" href="<?php  echo base_url("css/navigation.css?");?>" />
 
-<link type="text/css" rel="stylesheet" media="screen" href="<?php  echo base_url("css/color.css?") . date("U");?>"/>
-<link type="text/css" rel="stylesheet" media="screen" href="<?php  echo base_url("css/popup.css?") . date("U");?>" />
+<link type="text/css" rel="stylesheet" media="screen" href="<?php  echo base_url("css/color.css?");?>"/>
+<link type="text/css" rel="stylesheet" media="screen" href="<?php  echo base_url("css/popup.css?") ;?>" />
 
-<link type="text/css" rel="stylesheet" media="print" href="<?php  echo base_url("css/print.css?") . date("U")?>" />
+<link type="text/css" rel="stylesheet" media="print" href="<?php  echo base_url("css/print.css?") ?>" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
 <!-- add any special style sheets for this page only -->
 <?php if(isset($styles) && !empty($styles)):?>
   <?php foreach($styles as $style):?>
-        <link  rel="stylesheet" href="<?php echo base_url("css/$style");?>?data=<?php echo date("i");?>"></link>
+        <link  rel="stylesheet" href="<?php echo base_url("css/$style");?>"></link>
   <?php endforeach;?>
 <?php endif; ?>
 
@@ -32,7 +32,7 @@ var root_url = '<?php  echo base_url();?>';
 <script type="text/javascript" src="https://code.jquery.com/ui/1.8.24/jquery-ui.min.js"></script>
 
 <!-- General Script  -->
-<script type="text/javascript" src="<?php  echo base_url("js/general.js?") . date("U");?>"></script>
+<script type="text/javascript" src="<?php  echo base_url("js/general.js?") ;?>"></script>
 
 <!-- Rich Text Editor Script -->
 <!-- TODO: add this only when on a rich-text editing page -->
@@ -43,21 +43,21 @@ var root_url = '<?php  echo base_url();?>';
 <?php endif; ?>
 
 <!-- TARGET-specific scripts -->
-<script type="text/javascript" src="<?php  echo base_url("js/teacher.js");?>?data=<?php echo date("i");?>"></script>
-<script type="text/javascript" src="<?php  echo base_url("js/student.js");?>?data=<?php echo date("i");?>"></script>
-<script type="text/javascript" src="<?php  echo base_url("js/template.js");?>?data=<?php echo date("i");?>"></script>
-<script type="text/javascript" src="<?php  echo base_url("js/benchmark.js");?>?data=<?php echo date("i");?>"></script>
-<script type="text/javascript" src="<?php  echo base_url("js/attendance.js");?>?data=<?php echo date("i");?>"></script>
-<script type="text/javascript" src="<?php  echo base_url("js/support.js");?>?data=<?php echo date("i");?>"></script>
-<script type="text/javascript" src="<?php  echo base_url("js/narrative.js");?>?data=<?php echo date("i");?>"></script>
-<script type="text/javascript" src="<?php  echo base_url("js/password.js");?>?data=<?php echo date("i");?>"></script>
-<script type="text/javascript" src="<?php  echo base_url("js/feedback.js");?>?data=<?php echo date("i");?>"></script>
-<script type="text/javascript" src="<?php  echo base_url("js/assignment.js");?>?data=<?php echo date("i");?>"></script>
-<script type="text/javascript" src="<?php  echo base_url("js/report.js");?>?data=<?php echo date("i");?>"></script>
+<script type="text/javascript" src="<?php  echo base_url("js/teacher.js");?>"></script>
+<script type="text/javascript" src="<?php  echo base_url("js/student.js");?>"></script>
+<script type="text/javascript" src="<?php  echo base_url("js/template.js");?>"></script>
+<script type="text/javascript" src="<?php  echo base_url("js/benchmark.js");?>"></script>
+<script type="text/javascript" src="<?php  echo base_url("js/attendance.js");?>"></script>
+<script type="text/javascript" src="<?php  echo base_url("js/support.js");?>"></script>
+<script type="text/javascript" src="<?php  echo base_url("js/narrative.js");?>"></script>
+<script type="text/javascript" src="<?php  echo base_url("js/password.js");?>"></script>
+<script type="text/javascript" src="<?php  echo base_url("js/feedback.js");?>"></script>
+<script type="text/javascript" src="<?php  echo base_url("js/assignment.js");?>"></script>
+<script type="text/javascript" src="<?php  echo base_url("js/report.js");?>"></script>
 
-<script type="text/javascript" src="<?php  echo base_url("js/legend.js");?>?data=<?php echo date("i");?>"></script>
+<script type="text/javascript" src="<?php  echo base_url("js/legend.js");?>"></script>
 <?php if(isset($scripts) && !empty($scripts)):?>
 <?php foreach($scripts as $script):?>
-<script type="text/javascript" src="<?php echo base_url("js/$script");?>?data=<?php echo date("i");?>"></script>
+<script type="text/javascript" src="<?php echo base_url("js/$script");?>"></script>
 <?php endforeach;?>
 <?php endif; ?>
