b1b95d113c6089a89072ce60c71619ffcca856a7
Chris Dart <chrisdart@cerebratorium.com>
2019-01-25 16:12:43 -0600

    Add library ID to student export

--- application/views/student/export.php
+++ application/views/student/export.php
@@ -1,37 +1,39 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 $filename = "student_export.csv";
-$header = "First Name,Nickname,Last Name,Gender,Birthday,Age,Current Grade,Enrollment Grade,Enrollment Year,Classroom,MS-Group,Classroom Teacher/Advisor,Humanities Teacher,Email,Email Active";
-if($this->session->userdata("userID") == ROOT_USER){
-	$header .= ",Email Password";
+$header = "First Name,Nickname,Last Name,Gender,Birthday,Age,Current Grade,Enrollment Grade,Enrollment Year,Classroom,MS-Group,Classroom Teacher/Advisor,Humanities Teacher, Library ID,Email,Email Active";
+if ($this->session->userdata("userID") == ROOT_USER) {
+  $header .= ",Email Password";
 }
 $output = array($header);
-foreach($students as $student){
-	$line[] = $student->stuFirst;
-	$line[] = $student->stuNickname;
-	$line[] = $student->stuLast;
-	$line[] = $student->stuGender;
-	$line[] = format_date($student->stuDOB, "standard");
-	$line[] = get_age($student->stuDOB);
-	$line[] = format_grade($student->stuGrade);
-	$line[] = format_grade($student->baseGrade);
-	$line[] = $student->baseYear;
-	$line[] = $student->teachClass;
-	$line[] = $student->stuGroup;
-	$line[]= $student->teacherName;
-	$line[] = $student->humanitiesTeacher;
-	$line[] = $student->stuEmail;
-	if($student->stuEmailPermission == 1){
-		$line[] = "Yes";
-	}else{
-		$line[] = "No";
-	}
-	if($this->session->userdata("userID") == ROOT_USER){
-		$line[] = $student->stuEmailPassword;
-	}
+foreach ($students as $student) {
+  $line[] = $student->stuFirst;
+  $line[] = $student->stuNickname;
+  $line[] = $student->stuLast;
+  $line[] = $student->stuGender;
+  $line[] = format_date($student->stuDOB, "standard");
+  $line[] = get_age($student->stuDOB);
+  $line[] = format_grade($student->stuGrade);
+  $line[] = format_grade($student->baseGrade);
+  $line[] = $student->baseYear;
+  $line[] = $student->teachClass;
+  $line[] = $student->stuGroup;
+  $line[] = $student->teacherName;
+  $line[] = $student->humanitiesTeacher;
+  $line[] = $student->stuLibraryID;
+  $line[] = $student->stuEmail;
+  if ($student->stuEmailPermission == 1) {
+    $line[] = "Yes";
+  }
+  else {
+    $line[] = "No";
+  }
+  if ($this->session->userdata("userID") == ROOT_USER) {
+    $line[] = $student->stuEmailPassword;
+  }
 
-	$output[] = implode(",", $line);
-	$line = NULL;
+  $output[] = implode(",", $line);
+  $line = NULL;
 }
 
 $data = implode("\n", $output);
