e2e622939e674dbc2adb6ebe91a62368b59a8855
Chris Dart <chris@ten7.com>
2021-02-13 10:29:59 -0600

    Issue #51 add list options to tinymce dialog

--- js/tinymce.js
+++ js/tinymce.js
@@ -1,7 +1,11 @@
 tinymce.init({
 	selector: "textarea.tinymce",
-	plugins: "quickbars",
-	toolbar: true,
+	plugins: [
+		'advlist lists paste code help wordcount'
+	],
+	toolbar: 'undo redo | ' +
+'bold italic | bullist numlist | ' +
+'removeformat | help',
 	menubar: false,
 	browser_spellcheck: true,
 });
